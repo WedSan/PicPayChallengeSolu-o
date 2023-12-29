8e886b124a3c4e772469782d04441317fb01c108
WedSan <andre.alves.silvazesez@gmail.com>
2023-12-28 23:20:58 -0300

    Create Exceptions class for duplicate email

---
+++ src/main/java/com/desafiopicpayandrelson/desafiopicpay/exception/DuplicateUserDocumentException.java
@@ -0,0 +1,8 @@
+package com.desafiopicpayandrelson.desafiopicpay.exception;
+
+public class DuplicateUserDocumentException extends RuntimeException{
+
+    public DuplicateUserDocumentException(String message) {
+        super(message);
+    }
+}
