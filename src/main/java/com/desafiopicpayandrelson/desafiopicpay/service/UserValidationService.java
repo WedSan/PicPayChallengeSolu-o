e843038a40b2063c9cdad76568f6b7cabc714b0c
WedSan <andre.alves.silvazesez@gmail.com>
2023-12-29 22:48:37 -0300

    Create User Service Implementation

---
+++ src/main/java/com/desafiopicpayandrelson/desafiopicpay/service/UserValidationService.java
@@ -0,0 +1,8 @@
+package com.desafiopicpayandrelson.desafiopicpay.service;
+
+import com.desafiopicpayandrelson.desafiopicpay.dto.UserDto;
+
+public interface UserValidationService {
+
+    void validate(UserDto userDto);
+}
