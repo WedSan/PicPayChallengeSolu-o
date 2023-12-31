1c46803fdd48fc9af3dcc4a9f547e8cbeb53b15c
WedSan <andre.alves.silvazesez@gmail.com>
2023-12-31 11:56:28 -0300

    Create TreansactionValidator Interface

--- src/main/java/com/desafiopicpayandrelson/desafiopicpay/service/transaction/TransactionValidator.java
+++ src/main/java/com/desafiopicpayandrelson/desafiopicpay/service/transaction/TransactionValidator.java
@@ -1,9 +1,12 @@
 package com.desafiopicpayandrelson.desafiopicpay.service.transaction;
 
 import com.desafiopicpayandrelson.desafiopicpay.dto.TransactionDto;
+import com.desafiopicpayandrelson.desafiopicpay.model.User.UserEntity;
+
+import java.math.BigDecimal;
 
 public interface TransactionValidator {
 
-    void validate(TransactionDto transactionDto);
+    void validate(UserEntity sender, UserEntity receiver, BigDecimal amount);
 
 }
