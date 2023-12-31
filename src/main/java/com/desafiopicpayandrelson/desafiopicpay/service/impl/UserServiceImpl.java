9a2b3c336c690c4c683b42b938413fc978086b05
WedSan <andre.alves.silvazesez@gmail.com>
2023-12-31 12:02:24 -0300

    fix: Resolve issue with create method not setting name and document to UserEntity object

--- src/main/java/com/desafiopicpayandrelson/desafiopicpay/service/impl/UserServiceImpl.java
+++ src/main/java/com/desafiopicpayandrelson/desafiopicpay/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.desafiopicpayandrelson.desafiopicpay.service.impl;
 
 import com.desafiopicpayandrelson.desafiopicpay.dto.UserDto;
+import com.desafiopicpayandrelson.desafiopicpay.exception.UserIdNotFoundException;
 import com.desafiopicpayandrelson.desafiopicpay.model.User.UserEntity;
 import com.desafiopicpayandrelson.desafiopicpay.repository.UserEntityRepository;
 import com.desafiopicpayandrelson.desafiopicpay.service.UserService;
@@ -8,22 +9,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.xml.transform.stream.StreamSource;
+import java.util.Optional;
+
 @Service
 public class UserServiceImpl implements UserService {
 
-    private UserEntityRepository userRepository;
+    private final UserEntityRepository userRepository;
 
 
-    private PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(PasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserEntityRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public UserEntity create(UserDto userDto) {
         UserEntity userEntity = new UserEntity();
+        userEntity.setName(userDto.name());
+        userEntity.setDocument(userDto.document());
         userEntity.setEmail(userDto.email());
         userEntity.setBalance(userDto.balance());
         String passwordEncoded = this.passwordEncoder.encode(userDto.password());
@@ -32,6 +39,13 @@ public class UserServiceImpl implements UserService {
         return userEntity;
     }
 
+    @Override
+    public UserEntity findUser(Long id) {
+        Optional<UserEntity> userEntityOptional = this.userRepository.findUserEntityById(id);
+        if(userEntityOptional.isEmpty())
+            throw new UserIdNotFoundException("Doesn't exist user "+id);
+        else return userEntityOptional.get();
+    }
 
     @Override
     public UserEntity save(UserEntity userEntity) {
