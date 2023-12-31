a9c795d2c7eae8b708f41ba37119af0da687fc5f
WedSan <andre.alves.silvazesez@gmail.com>
2023-12-31 11:56:12 -0300

    feat: Create Authorization Service

--- src/main/java/com/desafiopicpayandrelson/desafiopicpay/model/Transaction.java
+++ src/main/java/com/desafiopicpayandrelson/desafiopicpay/model/Transaction.java
@@ -23,4 +23,54 @@ public class Transaction {
     private UserEntity receiver;
 
     private LocalDateTime timestamp;
+
+
+    public Transaction(){}
+
+    public Transaction(UserEntity sender, UserEntity receiver, BigDecimal amount) {
+        this.amount = amount;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public UserEntity getSender() {
+        return sender;
+    }
+
+    public void setSender(UserEntity sender) {
+        this.sender = sender;
+    }
+
+    public UserEntity getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(UserEntity receiver) {
+        this.receiver = receiver;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 }
