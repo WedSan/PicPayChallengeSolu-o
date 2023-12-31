d2cfee740048ba14876f86de63b8a698305de71a
WedSan <andre.alves.silvazesez@gmail.com>
2023-12-31 12:00:24 -0300

    enhancement: Implement findUserEntityById in UserEntityRepository

--- src/main/java/com/desafiopicpayandrelson/desafiopicpay/repository/UserEntityRepository.java
+++ src/main/java/com/desafiopicpayandrelson/desafiopicpay/repository/UserEntityRepository.java
@@ -3,10 +3,14 @@ package com.desafiopicpayandrelson.desafiopicpay.repository;
 import com.desafiopicpayandrelson.desafiopicpay.model.User.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
 
     boolean existsByEmail(String email);
 
     boolean existsByDocument(String document);
+
+    Optional<UserEntity> findUserEntityById(Long id);
 
 }
