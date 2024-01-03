308be8075dc32cb568e07fd098e74ee6dd8281b2
WedSan <andre.alves.silvazesez@gmail.com>
2024-01-03 19:23:42 -0300

    fix: Add repository annotation

--- src/main/java/com/desafiopicpayandrelson/desafiopicpay/repository/TransactionRepository.java
+++ src/main/java/com/desafiopicpayandrelson/desafiopicpay/repository/TransactionRepository.java
@@ -2,6 +2,8 @@ package com.desafiopicpayandrelson.desafiopicpay.repository;
 
 import com.desafiopicpayandrelson.desafiopicpay.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
