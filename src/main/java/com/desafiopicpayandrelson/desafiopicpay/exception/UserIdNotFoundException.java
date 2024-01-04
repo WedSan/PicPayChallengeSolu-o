2229a71fe82536b776b2a21cc15074ddd815260f
WedSan <andre.alves.silvazesez@gmail.com>
2024-01-03 21:14:20 -0300

    Fix: Create super constructor to UserIdNotFoundException constructor

--- src/main/java/com/desafiopicpayandrelson/desafiopicpay/exception/UserIdNotFoundException.java
+++ src/main/java/com/desafiopicpayandrelson/desafiopicpay/exception/UserIdNotFoundException.java
@@ -1,6 +1,7 @@
 package com.desafiopicpayandrelson.desafiopicpay.exception;
 
 public class UserIdNotFoundException extends RuntimeException {
-    public UserIdNotFoundException(String s) {
+    public UserIdNotFoundException(String message) {
+        super(message);
     }
 }
