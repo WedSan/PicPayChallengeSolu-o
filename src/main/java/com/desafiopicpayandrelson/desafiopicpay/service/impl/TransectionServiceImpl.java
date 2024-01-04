2cb769f608328223ac4b9bb6e87410675d6641cb
WedSan <andre.alves.silvazesez@gmail.com>
2024-01-03 21:11:03 -0300

    Fix: Set balance atualization to sender and receiver

--- src/main/java/com/desafiopicpayandrelson/desafiopicpay/service/impl/TransectionServiceImpl.java
+++ src/main/java/com/desafiopicpayandrelson/desafiopicpay/service/impl/TransectionServiceImpl.java
@@ -1,23 +1,15 @@
 package com.desafiopicpayandrelson.desafiopicpay.service.impl;
 
 import com.desafiopicpayandrelson.desafiopicpay.dto.TransactionDto;
-import com.desafiopicpayandrelson.desafiopicpay.exception.AuthorizationException;
-import com.desafiopicpayandrelson.desafiopicpay.exception.InvalidAuthorizationResponseException;
-import com.desafiopicpayandrelson.desafiopicpay.exception.TransactionNotAuthorizedException;
 import com.desafiopicpayandrelson.desafiopicpay.model.Transaction;
 import com.desafiopicpayandrelson.desafiopicpay.model.User.UserEntity;
-import com.desafiopicpayandrelson.desafiopicpay.model.response.AuthorizationResponse;
 import com.desafiopicpayandrelson.desafiopicpay.repository.TransactionRepository;
+import com.desafiopicpayandrelson.desafiopicpay.service.NotificationService;
 import com.desafiopicpayandrelson.desafiopicpay.service.TransactionService;
 import com.desafiopicpayandrelson.desafiopicpay.service.UserService;
 import com.desafiopicpayandrelson.desafiopicpay.service.transaction.TransactionValidator;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.Disposable;
-import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -33,24 +25,32 @@ public class TransectionServiceImpl implements TransactionService {
 
     private final AuthorizationService authorizationService;
 
+
+
     @Autowired
     public TransectionServiceImpl(TransactionRepository transactionRepository, UserService userService, List<TransactionValidator> transactionValidators, AuthorizationService authorizationService) {
         this.transactionRepository = transactionRepository;
         this.userService = userService;
         this.transactionValidators = transactionValidators;
         this.authorizationService = authorizationService;
+
     }
 
     @Override
     public Transaction createTransaction(TransactionDto transactionDto) {
         UserEntity receiver = this.userService.findUser(transactionDto.sender());
         UserEntity sender = this.userService.findUser(transactionDto.receiver());
+
         BigDecimal transactionValue = transactionDto.value();
 
-        validateTransaction(receiver, sender , transactionValue);
+        validateTransaction(receiver, sender, transactionValue);
         authorizationService.authorizeTransaction(receiver, sender, transactionValue);
 
         Transaction transaction = new Transaction(receiver, sender, transactionValue);
+        transferBalance(sender, receiver, transactionValue);
+
+
+
         return this.transactionRepository.save(transaction);
     }
 
@@ -58,6 +58,12 @@ public class TransectionServiceImpl implements TransactionService {
         this.transactionValidators.forEach(tValidation -> tValidation.validate(receiver, sender, transactionValue));
     }
 
+    private void transferBalance(UserEntity sender, UserEntity receiver, BigDecimal value){
+        sender.setBalance(sender.getBalance().subtract(value));
+        receiver.setBalance(receiver.getBalance().add(value));
+        userService.save(sender);
+        userService.save(receiver);
+    }
 
 
 }
