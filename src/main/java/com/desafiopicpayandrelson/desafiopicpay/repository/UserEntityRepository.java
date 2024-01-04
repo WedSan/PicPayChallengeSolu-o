3dda58f1f996e58ecd99db13bdb4c294625d5430
WedSan <andre.alves.silvazesez@gmail.com>
2024-01-03 21:14:43 -0300

    Fix: Add repository annotation

--- src/main/java/com/desafiopicpayandrelson/desafiopicpay/repository/UserEntityRepository.java
+++ src/main/java/com/desafiopicpayandrelson/desafiopicpay/repository/UserEntityRepository.java
@@ -2,9 +2,11 @@ package com.desafiopicpayandrelson.desafiopicpay.repository;
 
 import com.desafiopicpayandrelson.desafiopicpay.model.User.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
 
     boolean existsByEmail(String email);
